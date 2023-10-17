980d2cd8cf02df48958aa309e1d734a416166447
Aisha Ayinde <aishaayinde@Aishas-MacBook-Air.local>
2023-10-17 19:14:16 +0100

    t

--- Netlify/functions/api.js
+++ Netlify/functions/api.js
@@ -322,16 +322,5 @@ router.post('/user/login', async (req, res) => {
 api.use("/api/", router)
 export const handler = serverless(api)
 
-// YOUR_BASE_DIRECTORY/netlify/functions/api.ts
 
-// import express, { Router } from "express";
-// import serverless from "serverless-http";
 
-// const api = express();
-
-// const router = Router();
-// router.get("/hello", (req, res) => res.send("Hello World!"));
-
-api.use("/api/", router);
-
-export const handler = serverless(api);
